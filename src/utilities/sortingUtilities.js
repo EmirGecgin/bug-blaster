d5c7acb0fc276d3aef9cb9f940e9524f90504d11
Emir Geçgin <119293209+EmirGecgin@users.noreply.github.com>
2026-01-01 19:52:42 +0300

    +yazım hatası düzeltildi

--- src/utilities/sortingUtilities.js
+++ src/utilities/sortingUtilities.js
@@ -1,6 +1,6 @@
 export const sortTicket = (tickets, preference)=>{
     switch(preference){
-        case "Hight to Low":
+        case "High to Low":
             return [...tickets].sort((a,b)=>b.priority.localeCompare(a.priority));
         case "Low to Hight":
             return [...tickets].sort((a,b)=>a.priority.localeCompare(b.priority));
